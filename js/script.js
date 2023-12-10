cd2be49137f10a9d1321b878a3071bf08db1c859
Sheikh <kenneth.sheikh@hotmail.com>
2023-12-10 18:57:02 +0100

    added menu-bar with click

--- js/script.js
+++ js/script.js
@@ -1,44 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Your JavaScript code here
-
-    toggle_popup_menu();
-    toggle_cart_popup();
+    if (menuIcon) {
+        menuIcon.addEventListener('click', function() {
+            // Your click event logic here
+            alert('Icon Clicked!');
+        });
+    } else {
+        console.error('Menu icon element not found.');
+    }
 });
-
-// popup menu button
-function toggle_popup_menu() {
-    document.getElementById("toggleBtn").addEventListener("click", function () {
-        let menu = document.getElementById("popup-box-id");
-
-        if (menu.style.visibility === "visible") {
-            menu.style.display = "none"
-            menu.style.visibility = "hidden"
-            document.body.style.overflow = "visible";
-        } else {
-            menu.style.display = "block";
-            menu.style.visibility = "visible";
-            document.body.style.overflow = "hidden";
-        }
-
-        document.body.classList.toggle('new-class');
-    });
-}
-
-function toggle_cart_popup() {
-    document.getElementById("checkout-btn-id").addEventListener("click", function () {
-        let menu = document.getElementById("cart-back-container-id");
-
-        if (menu.style.visibility === "visible") {
-            menu.style.display = "none"
-            menu.style.visibility = "hidden"
-            document.body.style.overflow = "visible";
-        } else {
-            menu.style.display = "block";
-            menu.style.visibility = "visible";
-            document.body.style.overflow = "hidden";
-        }
-
-        document.body.classList.toggle('new-class');
-    });
-}
