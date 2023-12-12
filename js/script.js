daaae28177a925c5fe3a91da5cce96eb52da0aa3
jakob <jakobhkrohn@gmail.com>
2023-12-12 22:55:41 +0100

    load javascript method on window load

--- js/script.js
+++ js/script.js
@@ -1,35 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    toggle_popup_menu();
-});
-
-
-/* this is java for title change on each page
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the <h1> element by its ID
-    const pageTitleElement = document.getElementById('page-title');
-
-    // Check if the element exists on the page
-    if (pageTitleElement) {
-        // Set the page title based on the current page
-        const currentPage = window.location.pathname;
-        switch (currentPage) {
-            case '/page1.html':
-                pageTitleElement.textContent = 'Page 1 Title';
-                break;
-            case '/page2.html':
-                pageTitleElement.textContent = 'Page 2 Title';
-                break;
-            case '/page3.html':
-                pageTitleElement.textContent = 'Page 3 Title';
-                break;
-            default:
-                pageTitleElement.textContent = 'Default Title';
-        }
-    }
-});
-
-*/
-
 function toggle_popup_menu() {
     document.getElementById("menu-icon").addEventListener("click", function () {
         console.log("toggle_popup_menu");
@@ -47,4 +15,8 @@ function toggle_popup_menu() {
 
         document.body.classList.toggle('new-class');
     });
+}
+
+window.onload = function () {
+    toggle_popup_menu();
 }
