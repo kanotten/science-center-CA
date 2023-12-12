f3f4bebe3e5ca868f9f7c74e25f3290fd189a6dd
jakob <jakobhkrohn@gmail.com>
2023-12-12 20:57:01 +0100

    menu button popup

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.getElementById('menu-icon');
-
-    if (menuIcon) {
-        menuIcon.addEventListener('click', function() {
-            // Your click event logic here
-            alert('Icon Clicked!');
-        });
-    } else {
-        console.error('Menu icon element not found.');
-    }
+    toggle_popup_menu();
 });
 
 
@@ -38,3 +29,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 */
+
+function toggle_popup_menu() {
+    document.getElementById("menu-icon").addEventListener("click", function () {
+        console.log("toggle_popup_menu");
+        let menu = document.getElementById("popup-box-id");
+
+        if (menu.style.visibility === "visible") {
+            menu.style.display = "none"
+            menu.style.visibility = "hidden"
+            document.body.style.overflow = "visible";
+        } else {
+            menu.style.display = "block";
+            menu.style.visibility = "visible";
+            document.body.style.overflow = "hidden";
+        }
+
+        document.body.classList.toggle('new-class');
+    });
+}
