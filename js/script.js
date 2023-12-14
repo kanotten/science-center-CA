6d4a6b137958ed359af706444d0a4d9229e93e4c
Sheikh <kenneth.sheikh@hotmail.com>
2023-12-15 00:12:59 +0100

    structure fix

--- js/script.js
+++ js/script.js
@@ -2,49 +2,28 @@ function toggle_popup_menu() {
     document.getElementById("menu-icon").addEventListener("click", function () {
         console.log("toggle_popup_menu");
         let menu = document.getElementById("popup-box-id");
-        let overlay = document.querySelector(".overlay");
 
-        if (menu.style.visibility === "visible") {
+        if (menu.style.display === "block") {
             hideMenu();
         } else {
             showMenu();
         }
-
-        document.body.classList.toggle('new-class');
     });
+}
 
-    function showMenu() {
-        let menu = document.getElementById("popup-box-id");
-        let overlay = document.querySelector(".overlay");
+function showMenu() {
+    let menu = document.getElementById("popup-box-id");
 
-        menu.style.display = "block";
-        menu.style.visibility = "visible";
-        document.body.style.overflow = "hidden";
-        overlay.style.display = "block"; // Show the overlay
-    }
-
-    function hideMenu() {
-        let menu = document.getElementById("popup-box-id");
-        let overlay = document.querySelector(".overlay");
-
-        menu.style.display = "none";
-        menu.style.visibility = "hidden";
-        document.body.style.overflow = "visible";
-        overlay.style.display = "none"; // Hide the overlay
-    }
+    menu.style.display = "block";
 }
 
 function hideMenu() {
     let menu = document.getElementById("popup-box-id");
-    let overlay = document.querySelector(".overlay");
 
     menu.style.display = "none";
-    menu.style.visibility = "hidden";
-    document.body.style.overflow = "visible";
-    overlay.style.display = "none"; // Hide the overlay
 }
 
 window.onload = function () {
-    hideMenu(); // Ensure the menu is initially hidden
+    hideMenu();
     toggle_popup_menu();
 }
